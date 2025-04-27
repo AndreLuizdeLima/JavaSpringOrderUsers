1179dc1f0c51ded177fbfafeaf21c8ae2a842d77
Andre <andreluizjp2015@gmail.com>
2025-04-27 19:56:16 -0300

    first commit

---
+++ src/main/java/com/andre/usandoSpring/UsandoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.andre.usandoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsandoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsandoSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/andre/usandoSpring/UsandoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andre.usandoSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsandoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
