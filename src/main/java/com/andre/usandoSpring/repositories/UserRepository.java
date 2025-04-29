5cc1449549161b317f6a25f5badbeb501ebe6517
Andre <andreluizjp2015@gmail.com>
2025-04-28 21:07:04 -0300

    28042025 add class order

--- src/main/java/com/andre/usandoSpring/repositories/UserRepository.java
+++ src/main/java/com/andre/usandoSpring/repositories/UserRepository.java
@@ -1,4 +1,8 @@
 package com.andre.usandoSpring.repositories;
 
-public class UserRepository {
+import com.andre.usandoSpring.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+
 }
